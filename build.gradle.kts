5d6dccd0c8ab08dc9a8893fec38812721b1be692
namlev <mikipatkowski@protonmail.com>
2025-07-15 07:55:53 +0200

    Add paths

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webmvc")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux:4.3.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
---
+++ src/main/kotlin/com/coursy/gateway/config/GatewayConfig.kt
@@ -0,0 +1,52 @@
+package com.coursy.gateway.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class GatewayConfig {
+    @Value("\${services.auth.url:http://auth-service:8080}")
+    private lateinit var authServiceUrl: String
+
+    @Value("\${services.users.url:http://users-service:8080}")
+    private lateinit var usersServiceUrl: String
+
+    @Value("\${services.platforms.url:http://platforms-service:8080}")
+    private lateinit var platformsServiceUrl: String
+
+    @Value("\${services.courses.url:http://courses-service:8080}")
+    private lateinit var coursesServiceUrl: String
+
+    @Value("\${services.videos.url:http://videos-service:8080}")
+    private lateinit var videosServiceUrl: String
+
+    @Bean
+    fun routeLocator(builder: RouteLocatorBuilder): RouteLocator {
+        
+        return builder.routes()
+            .route("auth-service") { r ->
+                r.path("/auth/**")
+                    .uri(authServiceUrl)
+            }
+            .route("users-service") { r ->
+                r.path("/users/**")
+                    .uri(usersServiceUrl)
+            }
+            .route("platforms-service") { r ->
+                r.path("/platforms/**")
+                    .uri(platformsServiceUrl)
+            }
+            .route("courses-service") { r ->
+                r.path("/courses/**")
+                    .uri(coursesServiceUrl)
+            }
+            .route("videos-service") { r ->
+                r.path("/videos/**")
+                    .uri(videosServiceUrl)
+            }
+            .build()
+    }
+}
